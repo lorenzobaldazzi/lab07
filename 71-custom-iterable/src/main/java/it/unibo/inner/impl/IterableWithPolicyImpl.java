39f0df08ce4f0899cf83dda20a9318f5de1ae61f
Lorenzo Baldazzi <lorenzo.baldazzi2@studio.unibo.it>
2025-11-09 20:57:39 +0100

    completamento parte 2: iterable with policy

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -9,17 +9,37 @@ import it.unibo.inner.api.IterableWithPolicy;
 public class IterableWithPolicyImpl <T> implements IterableWithPolicy<T> {
     
     private final T[] elements;
+    private Predicate<T> filter;
 
     public IterableWithPolicyImpl(final T[] elements){
+        this(
+            elements,
+            new Predicate<T>() {
+                @Override
+                public boolean test(T elem){
+                    return true;
+                }
+            }
+        );
+
+    }
+
+
+    public IterableWithPolicyImpl(final T[] elements, Predicate<T> predicate){
         this.elements = elements;
+        this.filter = predicate;
 
     }
 
-    @Override
+
     public void setIterationPolicy(final Predicate<T> filter){
-        //TODO
+        this.filter = filter;
     }
 
+    public Iterator<T> iterator(){
+        return new IteratorImpl();
+    }
+    
     private class IteratorImpl implements Iterator<T> {
 
         private int index = 0;
@@ -28,7 +48,14 @@ public class IterableWithPolicyImpl <T> implements IterableWithPolicy<T> {
 
         @Override
         public boolean hasNext() {
-            return index < elements.length;
+            while(index < elements.length){
+                if(filter.test(elements[index])){
+                    return true;
+                }
+                index++;
+            }
+            return false;
+            
         }
 
         @Override
@@ -41,7 +68,7 @@ public class IterableWithPolicyImpl <T> implements IterableWithPolicy<T> {
 
     }
 
-    public Iterator<T> iterator(){
-        return new IteratorImpl();
-    }
+
+
+    
 }
